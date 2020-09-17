e27e3f8e127dda52e7463ae1f02541aabdc7ddf5
Sotatek-ThienNguyen2 <thien.nguyen2@sotatek.com>
2020-09-17 18:22:30 +0700

    add web development

--- build/js/our-services.js
+++ build/js/our-services.js
@@ -21,6 +21,24 @@ if (window.location.hash.substring(1).length) {
    document.getElementById("software-development").style.display = "block";
 }
 
+var tabsModuleS = document.body.querySelector("#ns-TabsModule-s");
+//get tab nav
+var tabNavList = document.body.querySelector(".ns-TabNav-s");
+//get all tab nav links
+var tabNavLinksS = document.querySelectorAll(".ns-TabNav_Link-s");
+//get tab nav current nav link indicator
+var tabNavCurrentLinkindicatorS = tabNavList.querySelector(".ns-TabNav_Indicator-s");
+//get all tab panels
+var tabPanelsS = document.querySelectorAll(".ns-TabPanel-s");
+//show tab panel A first because panel A nav link has indicator on page load
+if (window.location.hash.substring(1).length) {
+   document.getElementById(window.location.hash.substring(1)).style.display = "block";
+   console.log(window.location.hash.substring(1));
+   tabsModuleS.setAttribute("data-active-tab", window.location.hash.substring(1))
+} else {
+   document.getElementById("software-development").style.display = "block";
+}
+
 /**
 * position indicator function
 */
@@ -50,9 +68,9 @@ function positionIndicator() {
 */
 function hideAllTabPanels() {
    //loop through all tab panel elements
-   for (i = 0; i < tabPanels.length; i++) {
+   for (i = 0; i < tabPanelsS.length; i++) {
       //remove style attribute from all tab panels to hide them
-      tabPanels[i].removeAttribute("style");
+      tabPanelsS[i].removeAttribute("style");
    }
 };
 
@@ -61,6 +79,7 @@ function hideAllTabPanels() {
 * tab nav link event displays matching tab panel,
 * and positions current tab nav link indicator
 */
+let menuTab = ['software-development', 'ai-']
 var tabNavLinkEvent = function () {
    //get this link data value
 
@@ -89,6 +108,39 @@ var tabNavLinkEvent = function () {
   
 
 };
+var tabNavLinkEventS = function () {
+   //get this link data value
+
+   var thisLink = this.getAttribute("data-tab");
+   //get this link href value
+   var thisHref = this.getAttribute("href");
+   //get tab panel element with ID that matches this link href value
+   var thisTabPanel = document.querySelector(thisHref);
+   //set tab module parent data to this link data value
+   tabsModuleS.setAttribute("data-active-tab", thisLink);
+   //fire hide all tab panels function
+   hideAllTabPanels();
+   //get tab panel element with ID that matches this link href value and set its style to show it
+   thisTabPanel.style.display = "block";
+   ;
+   
+   AOS.refresh()
+   console.log(thisLink);
+   event.preventDefault()
+   if(history.pushState) {
+      history.pushState(null, null,  '#' + thisLink);
+  }
+  else {
+   window.location.hash = '#' + thisLink
+  }
+  
+
+};
+
+for (var i = 0; i < tabNavLinksS.length; i++) {
+   //for each nav link, add click event that fires tab nav link click event function
+   tabNavLinksS[i].addEventListener("click", tabNavLinkEventS, false);
+}
 
 for (var i = 0; i < tabNavLinks.length; i++) {
    //for each nav link, add click event that fires tab nav link click event function
